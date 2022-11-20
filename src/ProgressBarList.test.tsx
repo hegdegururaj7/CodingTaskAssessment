2c730df6a9206aa65b81941f7d4170f3e32fb25d
Gururaj Hegde <gururaj.h@genisys-group.com>
2022-11-20 15:00:16 +0530

    code changes and test fixes

--- src/ProgressBarList.test.tsx
+++ src/ProgressBarList.test.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import ProgressBarList from "./ProgressBarList/ProgressBarList";
+import ProgressBar from "./ProgressBar/ProgressBar";
+
+const mockChildComponent = jest.fn();
+jest.mock("./ProgressBar/ProgressBar", () => () => {
+  mockChildComponent();
+  return <mock-ProgressBar />;
+});
 
 test("should correctly set default option", () => {
   render(<ProgressBarList />);
@@ -15,9 +22,24 @@ test("should allow user to change progressBarOptions", () => {
   userEvent.selectOptions(
     // Find the select element
     screen.getByRole("progressBarItems"),
-    // Find and select the progressBarThree option
-    screen.getByRole("option", { name : "progressBarThree" })
+    // Find and select the progressBarTwo option
+    screen.getByRole("option", { name: "progressBarTwo" })
   );
-  expect(screen.getByText("progressBarThree")).toBeInTheDocument();
+  expect(screen.getByRole("option", { name: "progressBarTwo" }).selected).toBe(
+    true
+  );
+});
+
+test("should allow user to click  button and call child component", () => {
+  render(<ProgressBarList />);
+  render(<ProgressBar/>);
+  userEvent.selectOptions(
+    // Find the select element
+    screen.getByRole("progressBarItems"),
+    // Find and select the progressBarTwo option
+    screen.getByRole("option", { name: "progressBarTwo" })
+  );
+  userEvent.click(screen.getByRole("button", { name: "10" }));
+  expect(mockChildComponent).toHaveBeenCalled();
 });
 
--- src/ProgressBarList/ProgressBarList.tsx
+++ src/ProgressBarList/ProgressBarList.tsx
@@ -1,45 +1,84 @@
 import styles from "./ProgressBarList.module.scss";
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 import ProgressBar from "../ProgressBar/ProgressBar";
+
+export interface IProgressBarButtonInfo {
+  id: number;
+  progressBarWidth: number;
+}
 
 export interface IProgressBar {
   id: number;
   name: string;
+  width: number;
 }
-
 const progressBarList = [
-  { id: 1, name: "progressBarOne" },
-  { id: 2, name: "progressBarTwo" },
-  { id: 3, name: "progressBarThree" }
+  { id: 1, name: "progressBarOne", width: 25 },
+  { id: 2, name: "progressBarTwo", width: 50 },
+  { id: 3, name: "progressBarThree", width: 75 }
+];
+const progressBarButtonList = [
+  { id: 11, progressBarWidth: -10 },
+  { id: 12, progressBarWidth: -25 },
+  { id: 13, progressBarWidth: 10 },
+  { id: 14, progressBarWidth: 25 }
 ];
 
 export default function ProgressBarList() {
-  const [selectedProgressBar, selectProgressBar] = useState("1");
-  const [selectedProgressButton, selectProgressButton] = useState();
+  const [selectedProgressBar, selectProgressBar] = useState<number>(1);
+  const [progressBarListInt, setProgressBarList] = useState(progressBarList);
 
-  const onChangeHandler = (event:any) => {
-    selectProgressBar(event.target.value);
-    selectProgressButton(undefined);
+  const setWidth = (event: SyntheticEvent) => {
+    let temp = progressBarListInt.map((x: IProgressBar) => {
+      if (x.id === selectedProgressBar) {
+        let tempWidth = x.width + parseInt(event.target?.value);
+        x.width = tempWidth < 0 ? 0 : tempWidth;
+      }
+      return x;
+    });
+    setProgressBarList(temp);
+  };
+
+  const onChangeHandler = (event: any) => {
+    selectProgressBar(parseInt(event.target.value));
   };
   return (
     <>
-    <center><h2>Progress Bar Demo</h2></center>
-      <div>
-        <ProgressBar
-          progressBarValues ={{
-            progressBarButton: selectedProgressButton,
-            progressBarOptionId: selectedProgressBar
-          }}
-        />
+      <div className={styles.header}>
+        <h2>Progress Bar Demo</h2>
       </div>
-      <div className={styles.progressBarOptions}>
-        <select role="progressBarItems" onChange={onChangeHandler} className={styles.progressBar}>
+      <div>
+        {progressBarListInt.map((option: IProgressBar) => (
+          <ProgressBar width={option.width} />
+        ))}
+      </div>
+      <div className={styles.container}>
+        <select
+          role="progressBarItems"
+          onChange={onChangeHandler}
+          className={styles.progressBar}
+        >
           {progressBarList.map((option: IProgressBar) => (
-            <option role="option" key= {option.id} value={option.id}>
+            <option
+              role="option"
+              data-testid="selector"
+              key={option.id}
+              value={option.id}
+            >
               {option.name}
             </option>
           ))}
         </select>
+        {progressBarButtonList.map((option: IProgressBarButtonInfo) => (
+          <button
+            role="button"
+            key={option.id}
+            onClick={setWidth}
+            value={option.progressBarWidth}
+          >
+            {option.progressBarWidth}
+          </button>
+        ))}
       </div>
     </>
   );
